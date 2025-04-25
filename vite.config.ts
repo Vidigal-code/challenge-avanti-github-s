541e019afe87e4df21d1334106747848599ff464
Kauan Vidigal <kauanvidigalcontato@gmail.com>
2025-04-25 20:27:20 -0300

    fix add base gh pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+    base: '/challenge-avanti-github-s/',
     plugins: [react(),
-        tailwindcss()],
+        tailwindcss()]
 });
